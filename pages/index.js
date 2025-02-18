db4ab01297b0f4165baf168eca6b99c653c3e56f
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-17 22:20:26 -0500

    added gpa

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
 
     useIsomorphicLayoutEffect(() => {
         stagger(
-            [textOne.current, textTwo.current, textThree.current, textFour.current, textFive.current, texSix.current, texSeven.current],
+            [textOne.current, textTwo.current, textThree.current, textFour.current, textFive.current, texSix.current, texSeven.current ],
             {y: 40, x: -10, transform: "scale(0.95) skew(10deg)"},
             {y: 0, x: 0, transform: "scale(1)"}
         );
